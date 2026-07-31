4c53ef56a25a37ccd39b6537a4a84137f9591152
artemka2003 <86artem2003@mail.ru>
2026-07-31 21:45:37 +0300

    update SessionConfig - fix visibility timeout

--- src/main/java/ru/codeportfolio/config/SessionConfig.java
+++ src/main/java/ru/codeportfolio/config/SessionConfig.java
@@ -1,12 +1,18 @@
 package ru.codeportfolio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.jackson.SecurityJacksonModules;
+import org.springframework.session.config.SessionRepositoryCustomizer;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.session.data.redis.RedisSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tools.jackson.databind.json.JsonMapper;
+
+import java.time.Duration;
 
 @Configuration
 @EnableRedisHttpSession
@@ -19,4 +25,11 @@ public class SessionConfig {
 
         return new GenericJacksonJsonRedisSerializer(mapper);
     }
+
+    @Bean
+    public SessionRepositoryCustomizer<RedisSessionRepository> sessionTimeoutCustomizer(
+            @Value("${spring.session.timeout}") Duration timeout) {
+        return repository -> repository.setDefaultMaxInactiveInterval(timeout);
+    }
+
 }
